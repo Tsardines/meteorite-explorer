4013e0ab2308beeeea3fa9d8b18bbbc461f18df3
Haley <hjsulcer@gmail.com>
2019-05-27 17:19:31 -0400

    converted year-format to just year

---
+++ .history/meteorite-explorer/src/App_20190527171413.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Moment from 'react-moment';
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+
+    this.state = {
+      items: [],
+      value: ''
+    };
+    this.componentWillMount = this.componentWillMount.bind(this);
+  }
+
+  componentWillMount() {
+    fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({ items: data });
+    });
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ value });
+  };
+
+  render() {
+
+    const { items, value } = this.state;
+
+    return (
+      
+      <div className="wrapper">
+        <div className="header">Meteorite Explorer</div>
+
+        {/* <input type="text" placeholder="Enter search terms" value={value} onChange={this.handleChange} /> */}
+
+        <table className="my-table">
+          <thead>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Name Type</th>
+            <th>Rec Class</th>
+            <th>Mass (g)</th>
+            <th>Fall</th>
+            <th>Year</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+          </thead>
+
+          <tbody>
+          { items.map((item, num) => {
+            const year = {item.year}
+            return (
+              <tr>
+                <td><p key={num}>{item.name}</p></td>
+                <td><p key={num}>{item.id}</p></td>
+                <td><p key={num}>{item.nametype}</p></td>
+                <td><p key={num}>{item.recclass}</p></td>
+                <td><p key={num}>{item.mass}</p></td>
+                <td><p key={num}>{item.fall}</p></td>
+                <Moment>{year}</Moment>
+                <td><p key={num}>{item.reclat}</p></td>
+                <td><p key={num}>{item.reclong}</p></td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+      </div>
+    )
+  } // render
+
+}
+
+export default App;
